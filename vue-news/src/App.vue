b3db2ac3ddf2e183d7324c792fd1bc8953aca334
YunHak <sza0203@naver.com>
2021-09-01 22:54:16 +0900

    Vue CLI eslint 설정끄기

--- vue-news/src/App.vue
+++ vue-news/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    hi
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  fetchData() {
+    console.log('h');
+    console.log('as');
+    console.log('h');
   }
 }
 </script>

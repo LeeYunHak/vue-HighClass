d9280c2a4a791aabe024eb87a5f4c0700a7dfbed
YunHak <sza0203@naver.com>
2021-09-05 20:15:54 +0900

    NewsView,, actions, mutations적용

--- vue-news/src/views/NewsView.vue
+++ vue-news/src/views/NewsView.vue
@@ -1,28 +1,24 @@
 <template>
   <div>
-      <div v-for="user in users">{{ user.title }}</div>
+      <div v-for="user in this.$store.state.news">{{ user.title }}</div>
   </div>
 </template>
 
 <script>
-import { fetchNewsList } from '../api/index.js';
 export default {
-  data(){
-    return {
-      users: []
-    }
-  },
   created() {
-    console.log('호출 전  :', this);
-    fetchNewsList()
-    .then(response => {
-      console.log('호출 후', this);
-      this.users = response.data;
-    })
-    .catch(function(error){
-      console.log(error);
-    })
-  }
+    this.$store.dispatch('FETCH_NEWS');
+    // //1
+    // fetchNewsList()
+    // .then(response => {
+    //   console.log('호출 후', this);
+    //   //2
+    //   this.users = response.data;
+    // })
+    // .catch(function(error){
+    //   console.log(error);
+    // })
+  },
 }
 </script>
 
